c58da344a237b3727bd13b895598b0b1e6aabff5
Cj_dozie <126397480+Chidozie222@users.noreply.github.com>
2024-02-08 01:29:07 -0800

    Update TeacherAttendance.js

--- models/Admin/TeacherAttendance.js
+++ models/Admin/TeacherAttendance.js
@@ -4,12 +4,10 @@ const mongoose = require('mongoose')
 const TeacherAttendance = new mongoose.Schema(
     {
         Date: Date,
-        Teacher: {
-            TeacherFirstName: String,
-            TeacherLastName: String,
-            Absent: { type: Boolean, default: false },
-            Reason: String,
-        },
+        TeacherFirstName: String,
+        TeacherLastName: String,
+        Absent: { type: Boolean, default: false },
+        Reason: String,
         SchoolEmail: String
     },
     {
