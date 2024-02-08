1e24da8fa46b72be8afe83a61078cba293cd52be
Cj_dozie <126397480+Chidozie222@users.noreply.github.com>
2024-02-08 02:38:42 -0800

    Update StudentMark.js

--- models/Admin/StudentMark.js
+++ models/Admin/StudentMark.js
@@ -5,13 +5,11 @@ const StudentMark = new mongoose.Schema(
         Class: String,
         Examination: String,
         Subject: String,
-        Student: {
-            RollNumber: Number,
-            StudentFirstName: String,
-            StudentLastName: String,
-            Grade: String,
-            Remarks: String
-        },
+        RollNumber: Number,
+        StudentFirstName: String,
+        StudentLastName: String,
+        Grade: String,
+        Remarks: String,
         SchoolEmail: String
     },
     {
