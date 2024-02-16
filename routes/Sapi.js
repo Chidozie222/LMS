bde5ff4d3d4a9476d99ef16d7bae2951330c68a3
Cj_dozie <ibennejohn064@gmail.com>
2024-02-16 06:25:30 -0800

    Update Sapi.js

--- routes/Sapi.js
+++ routes/Sapi.js
@@ -65,7 +65,7 @@ Sapi.post('/student_and_parent', upload.fields([{ name: 'StudentPicture', maxCou
         console.log(UserByClassCapacity);
 
 
-        if (UserByClassCapacity) {
+        if (UserByClassCapacity != null) {
             if (UserByClass.length > UserByClassCapacity[0].ClassCapacity) {
             res.send({ status: 'error', message: 'The class is full' })
         } else if (UserBySchoolEmail.length > 0 && UserByStudentUsername.length > 0 && UserByParentUserName.length > 0 && UserByParentEmail.length > 0 && UserByStudentEmail.length > 0) {
