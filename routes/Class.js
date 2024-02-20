c7a00945b4d437fa1bddaa4e4b8c0804a8f18773
Cj_dozie222 <john@valuemize.co>
2024-02-20 01:18:21 -0800

    update

--- routes/Class.js
+++ routes/Class.js
@@ -4,7 +4,7 @@
 const { Router } = require("express");
 const mongoose = require('mongoose')
 
-const Router = Router()
+const classRoom = Router()
 
 // calling the schema 
 require('../models/Admin/Class')
@@ -12,35 +12,34 @@ require('../models/Admin/Class')
 // setting up the schema for the Class information backend
 const Classes = mongoose.model('Class')
 
-Router.get('/Class', async (req, res) => {
-    res.send({ message: "Hello" })
-    // const { Class, ClassNumber, ClassCapacity, ClassTeacher, ClassStartingOn, ClassEndingOn, ClassLocation, ClassFeeType, SchoolEmail } = req.body
-    // console.log(Class, ClassNumber, ClassCapacity, ClassTeacher, ClassStartingOn, ClassEndingOn, ClassLocation, ClassFeeType, SchoolEmail);
-    // try {
-    //     let User = await Classes.find({ SchoolEmail, Class })
-    //     if (!Class && !ClassNumber && !ClassCapacity && !ClassTeacher && !ClassStartingOn && !ClassEndingOn && !ClassLocation && !ClassFeeType && !SchoolEmail) { 
-    //         res.status(400).send({ message: "A value in not present" })
-    //     } else if (User && User.length > 0) {
-    //         res.send({ status: 'error', message: 'Sorry but User already exist' })
-    //     } else {
-    //         await Classes.create({
-    //             Class, 
-    //             ClassNumber, 
-    //             ClassCapacity, 
-    //             ClassTeacher, 
-    //             ClassStartingOn, 
-    //             ClassEndingOn, 
-    //             ClassLocation, 
-    //             ClassFeeType, 
-    //             SchoolEmail
-    //         })
-    //         res.send({ status: 'ok', message: 'Class successfully created' })
-    //     }
+classRoom.get('/Class', async (req, res) => {
+    const { Class, ClassNumber, ClassCapacity, ClassTeacher, ClassStartingOn, ClassEndingOn, ClassLocation, ClassFeeType, SchoolEmail } = req.body
+    console.log(Class, ClassNumber, ClassCapacity, ClassTeacher, ClassStartingOn, ClassEndingOn, ClassLocation, ClassFeeType, SchoolEmail);
+    try {
+        let User = await Classes.find({ SchoolEmail, Class })
+        if (!Class && !ClassNumber && !ClassCapacity && !ClassTeacher && !ClassStartingOn && !ClassEndingOn && !ClassLocation && !ClassFeeType && !SchoolEmail) { 
+            res.status(400).send({ message: "A value in not present" })
+        } else if (User && User.length > 0) {
+            res.send({ status: 'error', message: 'Sorry but User already exist' })
+        } else {
+            await Classes.create({
+                Class, 
+                ClassNumber, 
+                ClassCapacity, 
+                ClassTeacher, 
+                ClassStartingOn, 
+                ClassEndingOn, 
+                ClassLocation, 
+                ClassFeeType, 
+                SchoolEmail
+            })
+            res.send({ status: 'ok', message: 'Class successfully created' })
+        }
         
-    // } catch (error) {
-    //     res.send({ status: 'error', message: 'Error in the server' })
-    //     console.log(error)
-    // }
+    } catch (error) {
+        res.send({ status: 'error', message: 'Error in the server' })
+        console.log(error)
+    }
 })
 
-module.exports = Router
+module.exports = classRoom
