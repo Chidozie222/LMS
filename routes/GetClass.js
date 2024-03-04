a55e7f6413a91a420849a08f54ea9f9b92b7e740
Cj_dozie222 <john@valuemize.co>
2024-03-04 15:25:33 -0800

    Update GetClass.js

--- routes/GetClass.js
+++ routes/GetClass.js
@@ -11,6 +11,11 @@ require('../models/Admin/Class')
 // setting up the schema for the Class information backend
 const Classes = mongoose.model('Class')
 
+require('../models/Admin/teacher')
+
+// setting up the schema for the Student and Parent information backend
+const Teachers = mongoose.model('Teachers')
+
 
 // get Class data
 GetClass.get('/getClass/:SchoolEmail', async (req, res) => {
@@ -20,7 +25,13 @@ GetClass.get('/getClass/:SchoolEmail', async (req, res) => {
         let user = await Classes.find({ SchoolEmail })
 
         if (user && user.length > 0) {
-            res.send({status: 'ok', data: user})
+            let result = []
+            user.forEach(async(classTeacherNameAndId) => {
+                const { ClassTeacher } = classTeacherNameAndId
+                let teacherInfo = await Teachers.findById(ClassTeacher).select(['TeacherFirstName', 'TeacherLastName'])
+                result.push({ ...classTeacherNameAndId, "ClassTeacher": teacherInfo })
+            })
+            res.send({status: 'ok', data: result})
         } else {
             res.send({status: 'pending', message: 'No data found', data: []})
         }
